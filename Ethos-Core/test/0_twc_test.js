ba87f0968d93f4ab42c1385ed3a5e1cc1ad0d119
dazuiTian <505530098@qq.com>
2023-05-03 22:21:00 +0800

    commit changes

--- Ethos-Core/test/0_twc_test.js
+++ Ethos-Core/test/0_twc_test.js
@@ -10,7 +10,7 @@ describe("twcTest", function () {
         const CollateralConfig = await ethers.getContractFactory("CollateralConfig");
         collateralConfig = await CollateralConfig.deploy();
         await collateralConfig.deployed();
-        console.log("collateralConfig:" + collateralConfig.address);
+        console.log("collateralConfig contract address:" + collateralConfig.address);
       }
     
       before(async function () {
@@ -19,7 +19,15 @@ describe("twcTest", function () {
     
       // 
       it("test 0", async function () {
-       
+        // address[] calldata _collaterals,
+        // uint256[] calldata _MCRs,
+        // uint256[] calldata _CCRs 
+        let _collaterals = ["0x0000", "0x0001", "0x0002"];
+        let _MCRs = [100, 200, 300];
+        let _CCRs = [1000, 2000, 3000];
+        await collateralConfig.initialize(_collaterals, _MCRs, _CCRs);
+        let allowedCollaterals = await collateralConfig.getAllowedCollaterals();
+        console.log("allowedCollaterals:" + allowedCollaterals);  
       });
 
 
